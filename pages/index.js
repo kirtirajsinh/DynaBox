f0877643bac72d7be666053f0a6c02e8fa2d7e2a
Frederico Alves <fredericoalves.info@gmail.com>
2022-09-17 15:08:21 +0200

    landpage styling

--- pages/index.js
+++ pages/index.js
@@ -12,47 +12,43 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          HI
-        </h1>
         
         <div className={styles.logo_container}>
           <img className={styles.logo} src='/DYNABOX.png'></img>
         </div>
 
-
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          THE AWESOME NFT EXCHANGE GAME
         </p>
 
+        <h1 className={styles.title}>
+          <button className={styles.pushable}>
+            <span className={styles.shadow}></span>
+            <span className={styles.edge}></span>
+            <span className={styles.front}>
+              <img className={styles.metamask_logo} src='/metamask-icon.png'></img>
+              <span className={styles.button_text}>Connect Wallet</span>
+            </span>
+          </button>
+        </h1>
+
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="" className={styles.card}>
+            <h2>Locker &rarr;</h2>
+            <p>Check your NFT items</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="" className={styles.card}>
+            <h2>Gym &rarr;</h2>
+            <p>Upgrade your habilities</p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href=""
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h2>Ring &rarr;</h2>
+            <p>Fight to gain NFT rewards</p>
           </a>
         </div>
       </main>
@@ -63,10 +59,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Hachathon 2022
         </a>
       </footer>
     </div>
