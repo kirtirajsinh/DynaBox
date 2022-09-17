a8bf2e7ff93f356c68459b97e38614bba0e48b90
Frederico Alves <fredericoalves.info@gmail.com>
2022-09-17 23:37:32 +0200

    styling updated

---
+++ pages/ring.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const LockerPage = () => {
+    return (
+        <div className="flex flex-col h-screen text-white bg-gradient-linear from-blue-900 to-black">
+            <h1 className="text-4xl font-bold">Ring</h1>
+            <p>FIGHT FIGHT FIGHT</p>
+
+            <a href="./">&larr; Back</a>
+        </div>
+    )
+}
+
+export default LockerPage
