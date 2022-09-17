956bd021aa5db9c9a39453f053f70b0696f36251
Frederico Alves <fredericoalves.info@gmail.com>
2022-09-17 20:58:11 +0200

    Landing page style updated

--- components/LandingPage_3options.js
+++ components/LandingPage_3options.js
@@ -4,15 +4,15 @@ const LandingPage_3options = () => {
     return (
         <div className="inline-flex mt-9">
             <a href="#" className="text-white m-5 p-9 border-solid border-2 rounded-lg border-white transition-all hover:scale-110">
-                <h2 className="font-bold">Locker &rarr;</h2>
+                <h2 className="font-bold text-lg">Locker &rarr;</h2>
                 <p className="font-thin">Check your NFT items</p>
             </a>
             <a href="#" className="text-white m-5 p-9 border-solid border-2 rounded-lg border-white transition-all hover:scale-110">
-                <h2 className="font-bold">Gym &rarr;</h2>
+                <h2 className="font-bold text-lg">Gym &rarr;</h2>
                 <p className="font-thin">Upgrade your habilities</p>
             </a>
             <a href="#" className="text-white m-5 p-9 border-solid border-2 rounded-lg border-white transition-all hover:scale-110">
-                <h2 className="font-bold">Ring &rarr;</h2>
+                <h2 className="font-bold text-lg">Ring &rarr;</h2>
                 <p className="font-thin">Fight to gain NFT rewards</p>
             </a>
         </div>
---
+++ components/LockerPage.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const LockerPage = () => {
+    return (
+        <div className="flex flex-col h-screen items-center justify-center">
+            <h1 className="text-4xl font-bold">Locker</h1>
+        </div>
+    )
+}
+
+export default LockerPage
