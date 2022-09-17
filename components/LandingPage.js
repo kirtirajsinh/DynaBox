cb1d99cfa7728ba99658b7988a23d77a7019e3f0
Meta-K <kirtiraj3738@gmail.com>
2022-09-17 17:31:09 +0530

    fix

---
+++ components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ConnectWallet from './ConnectWallet'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <ConnectWallet />
+    </div>
+  )
+}
+
+export default LandingPage
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
 import '../styles/globals.css'
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+
+const { chains, provider } = configureChains(
+  [chain.polygonMumbai],
+  [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'DynaBox',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+  <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+          <Component {...pageProps} />
+      </RainbowKitProvider>
+      </WagmiConfig>
+  )
 }
 
 export default MyApp
